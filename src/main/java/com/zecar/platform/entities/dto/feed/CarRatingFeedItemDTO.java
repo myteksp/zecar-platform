af90a4bab0796f72d8ac1e4d99e544ba8288d0dc
Alex Ivanov <alex.ivanov@zecar.zone>
2018-12-06 14:48:44 +0200

    ZEC-409 | Add new attributes to feed items - add car rating value

--- src/main/java/com/zecar/platform/entities/dto/feed/CarRatingFeedItemDTO.java
+++ src/main/java/com/zecar/platform/entities/dto/feed/CarRatingFeedItemDTO.java
@@ -5,6 +5,10 @@ import io.swagger.annotations.ApiModelProperty;
 
 public final class CarRatingFeedItemDTO {
     @JsonProperty
+    @ApiModelProperty(notes="Current car rating")
+    public double rating;
+
+    @JsonProperty
     @ApiModelProperty(notes="Number of votes for this car")
     public int votes;
 
